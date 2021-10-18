46dafc2be70ccc46b76362d4736c70d50c86b46b
YumiMachino <y.y.1515m@gmail.com>
2021-10-17 18:15:36 -0700

    Add about, destination page

--- js/main.js
+++ js/main.js
@@ -10,6 +10,24 @@ const paginations = document.querySelectorAll(".pg");
 const testiPages = document.querySelectorAll(".testimonials-page");
 let index = 0;
 
+// FAQ
+const faq_angle_down_arrows = document.querySelectorAll(".fa-angle-down");
+const faq_angle_up_arrows = document.querySelectorAll(".fa-angle-up");
+const faqs = document.querySelectorAll(".faq");
+
+faq_angle_down_arrows.forEach((down_arrow) => {
+  down_arrow.addEventListener("click", (e) => toggleFAQ(e));
+});
+
+faq_angle_up_arrows.forEach((up_arrow) => {
+  up_arrow.addEventListener("click", (e) => toggleFAQ(e));
+});
+
+function toggleFAQ(e) {
+  const ele = e.target.parentElement.parentElement.parentElement;
+  ele.classList.toggle("active");
+}
+
 testi_left_arrow.addEventListener("click", () => {
   index--;
   slideTesti();
