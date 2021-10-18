1adca8b21ff0a6d378330ec8f1d68021cc3502af
YumiMachino <y.y.1515m@gmail.com>
2021-10-18 12:11:30 -0700

    Add features on destination

--- js/main.js
+++ js/main.js
@@ -15,6 +15,40 @@ const faq_angle_down_arrows = document.querySelectorAll(".fa-angle-down");
 const faq_angle_up_arrows = document.querySelectorAll(".fa-angle-up");
 const faqs = document.querySelectorAll(".faq");
 
+// Features
+const featureSections = document.querySelectorAll(".featured-section");
+const feature_left_arrow = document.querySelector(".f-left-arrow");
+const feature_right_arrow = document.querySelector(".f-right-arrow");
+const feature_num = document.querySelector(".slide-num");
+let featureInd = 0;
+
+feature_left_arrow.addEventListener("click", () => {
+  featureInd--;
+  toggleFeatures();
+});
+
+feature_right_arrow.addEventListener("click", () => {
+  featureInd++;
+  toggleFeatures();
+});
+
+function toggleFeatures() {
+  // remove "show" from all features
+  featureSections.forEach((feature) => {
+    feature.classList.remove("show");
+  });
+
+  // check index
+  if (featureInd < 0) {
+    featureInd = featureSections.length - 1;
+  } else if (featureInd > featureSections.length - 1) {
+    featureInd = 0;
+  }
+  // add "show"
+  featureSections[featureInd].classList.add("show");
+  feature_num.innerText = featureInd + 1;
+}
+
 faq_angle_down_arrows.forEach((down_arrow) => {
   down_arrow.addEventListener("click", (e) => toggleFAQ(e));
 });
